84c138dd5b68aab28fa3119dff5d75316aa8393f
jeffreybiles <bilesjeffrey@gmail.com>
2016-10-26 01:05:23 -0500

    Try to get monsters from server

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,18 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return [{
-      name: 'Sparkachu',
-      level: 3
-    }, {
-      name: 'Charember',
-      level: 29
-    }, {
-      name: 'Rhauk',
-      level: 5
-    }, {
-      name: 'Poddle',
-      level: 9
-    }]
+    return this.store.findAll('monster');
   }
 });
